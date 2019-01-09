3206fbd00998d6d27789546ebe976a27e657c5dc
sarahkittyy <chrisohlin1@gmail.com>
2019-01-09 10:12:43 -0500

    initial button implementation

--- header/GuiButton.h
+++ header/GuiButton.h
@@ -4,12 +4,70 @@
 
 #include "GuiComponent.h"
 #include "ResourceManager.h"
+#include "KeyManager.h"
 
+/**
+ * @brief Simple GUI Button class.
+ * 
+ */
 class GuiButton : public GuiComponent
 {
 public:
+	/**
+	 * @brief Default Constructor.
+	 * 
+	 */
 	GuiButton();
 
+	/**
+	 * @brief Called by the ResourceManager for initialization.
+	 * 
+	 * @param r The global ResourceManager.
+	 */
+	void rm_init(ResourceManager& r);
+
+	/**
+	 * @brief Main update function, call either by a parent object's updateRecursive() or call manually.
+	 * 
+	 */
+	void update();
+
+	/**
+	 * @brief Get whether or not the button is being pressed.
+	 * 
+	 * @return true if the button is pressed. 
+	 * @return false otherwise.
+	 */
+	bool isPressed();
+
 private:
+	/**
+	 * @brief SFML's internal draw() function.
+	 * 
+	 */
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+	/**
+	 * @brief The internal rectangle to be drawn.
+	 * 
+	 */
+	sf::RectangleShape mRect;
+
+	/**
+	 * @brief The texture when the button is pressed.
+	 * 
+	 */
+	sf::Texture* mTexPressed;
+
+	/**
+	 * @brief The texture when the button is unpressed.
+	 * 
+	 */
+	sf::Texture* mTexUnpressed;
+
+	/**
+	 * @brief For update(), prevents multiple repeated calls to setTexture().
+	 * 
+	 */
+	bool mWasPressed;
 };
--- src/GuiButton.cpp
+++ src/GuiButton.cpp
@@ -1,1 +1,54 @@
 #include "GuiButton.h"
+
+GuiButton::GuiButton()
+{
+	mWasPressed = false;
+}
+
+void GuiButton::rm_init(ResourceManager& r)
+{
+	//Init the textures.
+	mTexUnpressed = r.getTexture("resource/UI/PNG/blue_button07.png");
+	mTexPressed = r.getTexture("resource/UI/PNG/blue_button08.png");
+
+	//Init the internal rect.
+	mRect.setTexture(mTexUnpressed);
+	mRect.setSize(sf::Vector2f(50,50));
+}
+
+void GuiButton::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	states.transform *= getTransform();
+	target.draw(mRect, states);
+}
+
+void GuiButton::update()
+{
+	//Set the texture based on pressed state.
+	if(isPressed())
+	{
+		if(!mWasPressed)
+			mRect.setTexture(mTexPressed, true);
+		mWasPressed = true;
+	}
+	else if(mWasPressed)
+	{
+		mRect.setTexture(mTexUnpressed, true);
+		mWasPressed = false;
+	}
+}
+
+bool GuiButton::isPressed()
+{
+	return (
+		//If the mouse is pressed down &..
+		KeyManager::getMouseState() == 1 &&
+		//..if the mouse is inside the button boundary rect.
+		mRect.getLocalBounds().contains
+		(
+			static_cast<sf::Vector2f> (
+				KeyManager::getMousePos()
+			)
+		)
+	);
+}
